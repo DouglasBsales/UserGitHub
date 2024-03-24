3e2ed681d14892cb3ddc25b7f52e6ba7aba23668
Douglas barbosa sales <126630439+DouglasBsales@users.noreply.github.com>
2024-03-24 14:22:06 -0300

    update project

--- script.js
+++ script.js
@@ -12,54 +12,63 @@ function gitHubRepos() {
       }
 
       let data = await res.json();
-      console.log(data);
+
       data.forEach((elemento) => {
         let projetos = document.createElement("div");
+        let language =
+          elemento.language != null
+            ? elemento.language
+            : "Não possui uma linguagem";
         projetos.innerHTML = `
 
-        <div style="width: 1000px; display: flex; justify-content: space-between;  border-bottom: 1px solid #cccccc; padding: 20px 0px"
-        id="projects"
-        >
-        <div>
-            <a href="${
-              elemento.html_url
-            }" style="color: #0969da; font-size: 20px; font-weight: 700;  text-decoration: none;" target="_blank">
-              ${elemento.name}
-              <span
-                style="
-                  margin-left: 10px;
-                  padding: 5px 10px;
-                  color: #636C76;
-                  border-radius: 20px;
-                  border: 1px solid #cccccc;
-                  text-align: center;
-                  font-size: 12px;
-                "
-              >
-                ${elemento.visibility}
-              </span>
-              </a>
-              <p style="color: #636C76; font-size: 12px; padding-top: 10px; display: flex; align-items: center;">
-                <span style="display: inline-block; width: 15px; height: 15px; border: 1px solid #cccccc; border-radius: 100%; margin-right: 6px; background-color: #0969da">
-                    
-                </span>
-                ${elemento.language}
-                <span style="color: #636C76; padding-left: 20px; font-size: 12px;">
-                    ${Intl.DateTimeFormat("pt-br").format(
-                      new Date(elemento.pushed_at)
-                    )}
-                </span>
-              </p>
-        </div>
-        <div>
-            <div style="display: flex; margin-bottom: -9px;">
-                <p style="padding-left: 30px; border: 1px solid #cccccc; padding: 5px 20px; border-top-left-radius: 6px; background-color: #EEF1F4; border-bottom-left-radius: 6px; background-color: #EEF1F4; color: #636C76">Star</p>   
-                <p style="width: 30px; border-top-right-radius: 6px; background-color: #EEF1F4; border-bottom-right-radius: 6px; border: 1px solid #cccccc; background-color: #EEF1F4;">
-
-                </p>  
+        <div id="projects" class="w-[1000px] flex justify-between border-b border-[#cccccc]">
+            <div class="flex py-[30px]">
+              <div>
+                <a href="${
+                  elemento.html_url
+                }" class=" text-xl font-bold" target="_blank" style="color: #0969DA">
+                  ${elemento.name}
+                </a>
+                <div class="ml-[5px] py-[5px] px-[10px] border border-[#cccccc] inline-block rounded-[20px]">
+                  <p class="color-[#cccccc] text-xs">${elemento.visibility}</p>
+                </div>
+                <div class="flex items-center gap-[10px] pt-[10px]">
+                  <div class="flex items-center gap-[4px]">
+                    <div class="w-[15px] h-[15px] border border-[#cccccc] rounded-[100%] bg-[#cccccc]"></div>
+                    <p class="color-[#636C76] text-sm">${language}</p>
+                  </div>
+                  <div class="flex items-baseline justify-center gap-[3px]">
+                    <i class="bi bi-star text-sm"></i>
+                    <p class="color-[#636C76] text-xs">${
+                      elemento.stargazers_count
+                    }</p>
+                  </div>
+                  <div>
+                    <p class="color-[#636c76] text-xs">
+                      ${Intl.DateTimeFormat("pt-br").format(
+                        new Date(elemento.pushed_at)
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-      </div>
+            <div>
+              <div class="flex">
+                <div class="flex bg-[#eef1f4] border border-[#cccccc] gap-[10px] items-baseline mt-[30px] pl-[10px] pr-[10px] py-[3px] rounded-s-lg">
+                  <div><i class="bi bi-star text-sm"></i></div>
+                  <p class="color: #636c76; text-xs">Star</p>
+                </div>
+
+                <div class="flex w-[30px] bg-[#eef1f4] border-b border-t border-r border-[#cccccc] justify-center mt-[30px] rounded-e-lg items-center">
+                  <div>
+                    <i class="bi bi-caret-down-fill text-xs"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
         `;
 
         divprojects.appendChild(projetos);
@@ -81,51 +90,37 @@ function getPerfil() {
     }
 
     let data = await res.json();
-    let opcao2 = data.bio != null ? data.bio : "O usuário nao possui uma bio";
+    let bio = data.bio != null ? data.bio : "O usuário nao possui uma bio";
 
     let avatar = document.createElement("div");
     avatar.innerHTML = `
 
-    <img
-          src="${data.avatar_url}"
-          alt=""
-          style="
-            width: 350px;
-            height: 350px;
-            border: 1px solid #cccccc;
-            border-radius: 100%;
-            margin-top: 60px;
-            margin-right: 50px;
-          "
-        />
-
-        <div style="width: 350px">
-          <a href="${data.html_url}"
-            style="
-              font-size: 32px;
-              font-weight: 600;
-              margin-bottom: -20px;
-              color: #1f2328;
-              text-decoration: none;
-            "
-            target="_blank"
-          >
-            ${data.name}
-          </a>
-          <p style="font-size: 26px; color: #636c76; font-weight: lighter">
-            ${data.login}
-          </p>
-          <p style="width: 310px; color: #1f2328; font-size: 18px">
-           ${opcao2}
-          </p>
-          <div style="display: flex; gap: 6px;">
-            <p style="font-weight: bold">${data.followers}</p>
-            <p style="color: #636c76">followers</p>
-            <div style="margin-top: 12px;">.</div>
-            <p style="font-weight: bold">${data.following}</p>
-            <p style="color: #636c76">following </p>
+    <div class="flex justify-center">
+            <img
+              src="${data.avatar_url}"
+              alt=""
+              class="w-[350px] h-[350px] border border-[#cccccc] rounded-full"
+            />
           </div>
-        </div>
+
+          <div class="flex flex-col  items-center w-[350px]">
+            <a
+              href="${data.html_url}"
+              class="text-[32px] font-600 color-[#1f2328]"
+              target="_blank"
+            >
+              ${data.name}
+            </a>
+            <p class="text-[26px] color-[#636c76] font-light">${data.login}</p>
+            <p class=" text-center color-[#1f2328] text-lg pb-[20px] font-light pt-[10px]">${bio}</p>
+            <div class="flex gap-[6px] items-center">
+              <p class="font-bold">${data.followers}</p>
+              <p class="color-[#636c76]">followers</p>
+              <div class="border border-[#000] rounded-full"></div>
+              <p class="font-bold">${data.following}</p>
+              <p class="color-[#636c76]">following</p>
+            </div>
+          </div>
 
     `;
 
